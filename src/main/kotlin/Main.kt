6882d70a1fc25f48f04047149e1b320888dbe0e5
jmd28 <jmd28@st-andrews.ac.uk>
2022-10-30 18:05:08 +0000

    move?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 import processing.core.PApplet
 import processing.core.PVector
-import java.util.InputMismatchException
-import kotlin.math.cos
-import kotlin.math.sin
+import java.util.Spliterators
+import kotlin.math.*
 
+//import ejml
+//import org.ejml.
 
 // game loop driver fun
 //override fun draw() {
@@ -27,10 +28,8 @@ data class Input(
 )
 typealias SphericalCoords = PVector
 fun SphericalCoords.r() = this.z
-fun SphericalCoords.theta() = this.x
+fun SphericalCoords.theta(): Float = this.x
 fun SphericalCoords.phi() = this.y
-
-
 
 fun SphericalCoords.toXyz(): PVector {
     val x1 = sin(theta()) * cos(phi())
@@ -41,6 +40,35 @@ fun SphericalCoords.toXyz(): PVector {
     return ret
 }
 
+fun PVector.toSpherical(): SphericalCoords {
+    val r = sqrt(x.pow(2) + y.pow(2) + z.pow(2))
+    val theta = atan(x/z)
+    val phi = acos(y/r)
+    return SphericalCoords(theta, phi, r)
+}
+
+fun PVector.rotateAngleAxis(theta: Float, axis: PVector): PVector {
+    val axisNormalised = axis.copy().normalize()
+    val u = axisNormalised.x
+    val v = axisNormalised.y
+    val w = axisNormalised.z
+
+    val xPrime =
+        u * (u * x + v * y + w * z) * (1.0 - cos(theta)) + x * cos(theta) + (-w * y + v * z) * sin(
+            theta
+        )
+    val yPrime =
+        v * (u * x + v * y + w * z) * (1.0 - cos(theta)) + y * cos(theta) + (w * x - u * z) * sin(
+            theta
+        )
+    val zPrime =
+        w * (u * x + v * y + w * z) * (1.0 - cos(theta)) + z * cos(theta) + (-v * x + u * y) * sin(
+            theta
+        )
+    return PVector(xPrime.toFloat(), yPrime.toFloat(), zPrime.toFloat())
+
+}
+
 data class WallE(val pos: SphericalCoords, val size: Float = 5f, var rot: Float = 0f) {
     fun draw(app: PApplet) {
         val position = pos.toXyz()
@@ -48,26 +76,54 @@ data class WallE(val pos: SphericalCoords, val size: Float = 5f, var rot: Float 
             pushMatrix()
             translate(position.x,position.y,position.z)
             circle(0f,0f,size)
+            fill(255f,0f,0f)
+            circle(sin(rot),cos(rot),2f)
             popMatrix()
         }
     }
 
+    var angle = 0.0f
     fun update(input: Input) {
-        val movementSpeed = 0.01f
-        val lookSpeed = 0.01f
+//        val movementSpeed = 0.01f
+//        angle += 0.01f
+//        val lookSpeed = 0.01f
 
-        var isMoving = false
         when {
-            input.isUp -> isMoving = true
-//            input.isDown -> pos.x -= movementSpeed
-            input.isLeft -> rot += lookSpeed
-            input.isRight -> rot -= lookSpeed
+            input.isUp -> pos.x += 0.01f
+            input.isDown -> pos.x -= 0.01f
+            input.isLeft -> pos.y += 0.01f
+            input.isRight -> pos.y -= 0.01f
         }
 
-        // actually move!!!!
-        
 
-
+//        // actually move!!!!
+//        // translate to wallE position
+//        val wallEPos = pos.toXyz()
+////        val up = wallEPos.copy().normalize()
+////        wallEPos.x * up.x +
+//        // new point is wallE pos + 1 (in X let's say)
+////        val newPos = pos.copy().toXyz()
+////            .rotateAngleAxis(angle, PVector(0f,1f,0f))
+////            .toSpherical()
+//
+////        angle += 0.01f
+//
+//        val newPos = wallEPos
+////            // go to wallE reference frame
+////            .rotateAngleAxis(pos.theta(), PVector(0f,1f,0f))
+////            .rotateAngleAxis(pos.phi(), PVector(0f,0f,1f))
+////            .rotateAngleAxis(rot, PVector(0f,1f,0f))
+////            // step forward by 1
+////            .add(100f,0f,0f)
+////            .rotateAngleAxis(-rot, PVector(0f,1f,0f))
+////            // go back to world coords
+////            .rotateAngleAxis(-pos.phi(), PVector(0f,0f,1f))
+////            .rotateAngleAxis(-pos.theta(), PVector(0f,1f,0f))
+//            .toSpherical()
+//
+////        newPos.z = EARTH_RADIUS + 2
+//
+//        pos.set(newPos)
     }
 }
 
@@ -157,9 +213,13 @@ class Game : PApplet() {
         }
     }
 
+    var theta = 1f
+    var phi = 0f
+
     override fun draw() {
+        rotate(radians(90f))
         background(0f)
-        val eyepos =
+//        val eyepos =
 
 //        camera(cartWallE.x, cartWallE.y, cartWallE.z, eyepos.x, eyepos.y, eyepos.z, up.x,up.y,up.z)
         setupCam()
@@ -168,12 +228,30 @@ class Game : PApplet() {
 //        with(pos) {
 //            translate(x,y,z)
 //        }
+
         fill(0f, 0f, 200f)
+        pushMatrix()
+        rotateX(radians(90f))
         sphere(EARTH_RADIUS)
+        popMatrix()
         fill(255f)
+
+
+//        // test
+//        pushMatrix()
+////        theta+=0.1f
+//        phi+=0.1f
+//        val testPos = SphericalCoords(theta, phi, EARTH_RADIUS+1).toXyz()
+//        translate(testPos.x, testPos.y,testPos.z)
+//        circle(0f,0f,50f)
+//        popMatrix()
+
+
+
         wallE.draw(this)
         moon.draw(this)
         moon.pos.x += 0.01f
+
         wallE.update(input)
         popMatrix()
     }
